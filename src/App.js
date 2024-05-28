56d104ae5aecbdd496049071a89261722108efb3
ANDRES FELIPE FERNANDEZ GARCIA <affernandez999@gmail.com>
2024-05-27 21:04:52 -0500

    add modify person

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
+import { useState } from 'react';
+
 import { Persons } from './components/Persons';
 
 function App() {
+  const [persons, setPersons] = useState([
+    {
+      id: 1,
+      name: 'Andrés Fernández',
+      role: 'Frontend Developer',
+      img: 'https://bootdey.com/img/Content/avatar/avatar6.png',
+    },
+    {
+      id: 2,
+      name: 'Jennifer',
+      role: 'Backend Developer',
+      img: 'https://bootdey.com/img/Content/avatar/avatar3.png',
+    },
+    {
+      id: 3,
+      name: 'Mariela',
+      role: 'UI/UX Designer',
+      img: 'https://bootdey.com/img/Content/avatar/avatar8.png',
+    },
+  ]);
+
   return (
-    <div>
-      <Persons />
+    <div className="App">
+      <div className="container">
+        <Persons persons={persons} setPersons={setPersons} />
+      </div>
     </div>
   );
 }
--- src/components/Persons.jsx
+++ src/components/Persons.jsx
@@ -1,49 +1,100 @@
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
 import { Person } from './Person';
+import { useState } from 'react';
 
-export const Persons = () => {
+export const Persons = ({ persons, setPersons }) => {
+  const [editingId, setEditingID] = useState(null);
+  const [editedPerson, setEditedPerson] = useState({
+    name: '',
+    role: '',
+    img: '',
+  });
+  const [isEditing, setIsEditing] = useState(false);
 
-  const [ persons, setPersons ] = useState([
-    {
-      id: 1,
-      name: "Oscar Eduardo",
-      role: "Frontend Developer",
-      img: "https://bootdey.com/img/Content/avatar/avatar6.png",
-    },
-    {
-      id: 2,
-      name: "Carolina",
-      role: "Backend Developer",
-      img: "https://bootdey.com/img/Content/avatar/avatar3.png",
-    },
-    {
-      id: 3,
-      name: "Catalina",
-      role: "UI/UX Designer",
-      img: "https://bootdey.com/img/Content/avatar/avatar8.png",
-    },
-  ]);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEditedPerson((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    console.log(editedPerson);
+  };
+
+  const handleEdit = (id) => {
+    // Establece el ID de la persona que queremos editar
+    setEditingID(id);
+    // Activamos el estado de edición
+    setIsEditing(true);
+    // Buncamos la persona a editar por medio del id y la asigna a personToEdit
+    const personToEdit = persons.find((person) => person.id === id);
+    // Establecemos los datos de la persona a editar
+    setEditedPerson({ ...personToEdit });
+  };
+
+  const handleSave = (e) => {
+    // Actualizar el array de persons con los cambios que tenemos en el formulario
+    setPersons(persons.map((person) => (person.id === editingId ? editedPerson : person)));
+    // Resetear el ID a null, de la persona que ya se editó
+    setEditingID(null);
+    // Resetear los datos de la persona editada
+    setEditedPerson({ name: '', role: '', img: '' });
+    // Desactiva el estado de edición
+    setIsEditing(false);
+  };
 
   return (
     <div>
       <h2>IT Team</h2>
-      <div className='container d-flex justify-content-center '>
-        <div className='d-flex flex-row'>
+      <div className="container d-flex justify-content-center ">
+        <div className="d-flex flex-row">
           {persons.map((person) => {
             return (
-              <div>
+              <div key={person.id}>
                 <Person
-                  key={person.id}
                   name={person.name}
                   role={person.role}
                   img={person.img}
+                  handleEdit={() => handleEdit(person.id)}
                 />
               </div>
-            )
+            );
           })}
         </div>
       </div>
+      <div className="mt-4 container row">
+        <div className="col-md-8 mx-auto bg-light">
+          <h2>Modificar datos</h2>
+          <input
+            type="text"
+            name="name"
+            value={editedPerson.name}
+            onChange={handleChange}
+            placeholder="Nombre"
+            className="form-control mb-2"
+          />
+          <input
+            type="text"
+            name="role"
+            value={editedPerson.role}
+            onChange={handleChange}
+            placeholder="Rol"
+            className="form-control mb-2"
+          />
+          <input
+            type="text"
+            name="img"
+            value={editedPerson.img}
+            onChange={handleChange}
+            placeholder="URL de la imagen"
+            className="form-control mb-2"
+          />
+          <div className="mt-2">
+            <button className="btn btn-primary" onClick={handleSave}>
+              Guardar
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
